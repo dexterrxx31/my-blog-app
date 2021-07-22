998d50543b33917cfdb943e331b35467377ebd42
Riyan Ahmad <riyanahmad99@gmail.com>
2021-07-23 00:50:03 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 const app = express();
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -8,12 +9,21 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const posts = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/" , function(req,res){
-  res.render("home",{content:homeStartingContent});
+  res.render("home",{content:homeStartingContent , posts : posts});
+});
+app.get("/post/:eachPost" , function(req,res){
+posts.forEach(function(x){
+  if( _.lowerCase(x.title) === _.lowerCase(req.params.eachPost)){
+     res.render("post",{title:x.title , content : x.content});
+  }
+});
+
 });
 app.get("/about" , function(req,res){
   res.render("about",{aboutContent: aboutContent});
@@ -25,8 +35,12 @@ app.get("/compose" , function(req,res){
   res.render("compose");
 });
 app.post("/compose" , function(req,res){
-  console.log(req.body.newBlogContent);
-  res.redirect("/compose");
+  let post = {
+    title : req.body.postTitle , 
+    content : req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 app.listen(3000, function() {
   console.log("Server started on port 3000");
